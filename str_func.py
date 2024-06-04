c06286919a90b5af748c63c7c72a92e643e2571e
megaokna43 <fitobar43@gmail.com>
2024-06-04 06:27:28 +0300

    создал файл str_func.py

---
+++ str_func.py
@@ -0,0 +1,8 @@
+def convert_to_uppercase(text):
+    uppercase_text = text.upper()
+    return uppercase_text
+
+original_text = input("Введите любое слово маленькими буквами: ")
+uppercase_text = convert_to_uppercase(original_text)
+print(uppercase_text)  # Выведет введенное слово большими буквами
+
